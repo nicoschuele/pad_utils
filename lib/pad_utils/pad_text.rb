0d466633c6e2251630a3275c934fd3ecfaec1891
Nico Schuele -mini <nicoschuele@gmail.com>
2016-02-20 00:35:42 +0100

    Add signatures for text insert methods

--- lib/pad_utils/pad_text.rb
+++ lib/pad_utils/pad_text.rb
@@ -1,7 +1,7 @@
 module PadUtils
 
   # Convert a string into a proper Ruby name.
-  # For example, 'app_name' will be converted to 'AppName' 
+  # For example, 'app_name' will be converted to 'AppName'
   def self.convert_to_ruby_name(value)
     if value.scan(/\_|\-/).size > 0
       value.split(/\_|\-/).map(&:capitalize).join
@@ -9,7 +9,7 @@ module PadUtils
       value.slice(0,1).capitalize + value.slice(1..-1)
     end
   end
-  
+
   # Convert a string to only alphanumeric and underscores
   def self.sanitize(value)
     value.tr('^A-Za-z0-9', '_')
@@ -17,13 +17,29 @@ module PadUtils
 
   # Replace text within a file.
   # old_text can be a regex or a string
-  def self.replace(file, old_text, new_text)
+  def self.replace_in_file(file, old_text, new_text)
     text_update = File.read(file)
     text_update = text_update.gsub(old_text, new_text)
 
     File.open(file, "w") { |f| f.write(text_update) }
   rescue Exception => e
-    puts e
+    PadUtils.log("Error replacing #{old_text} in #{file} with #{new_text}", e)
+  end
+
+  def self.insert_before(original, tag, text)
+    # TODO: Implement
+  end
+
+  def self.insert_before_in_file(file, tag, text)
+    # TODO: Implement
+  end
+
+  def self.insert_after(original, tag, text)
+    # TODO: Implement
+  end
+
+  def self.insert_after_in_file(file, tag, text)
+    # TODO: Implement
   end
 
 end
