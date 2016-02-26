107c3c4d401d173453ab919f6251ba13c13fe52b
Nico Schuele -mini <nicoschuele@gmail.com>
2016-02-26 23:14:19 +0100

    Add YARD documentation for File utilities

--- lib/pad_utils/pad_files.rb
+++ lib/pad_utils/pad_files.rb
@@ -6,70 +6,144 @@ module PadUtils
   # Most are wrappers on FileUtils existing methods. Having them here,
   # a user of PadUtils doesn't have to remember names and parameters.
 
-  # Delete a file. If not found, doesn't raise any error.
+  # Deletes a file.
+  #
+  # @param file_path [String] the file path and name
+  # @return [Void] nothing
+  # @example
+  #   PadUtils.delete("path/to/file.txt")
   def self.delete_file(file_path)
     FileUtils.rm(file_path, force: true)
   end
 
-  # Just a wrapper on File.exist? for consistency.
+  # Checks if a file exists.
+  #
+  # @param file_path [String] the file path and name
+  # @return [Boolean]
+  # @example
+  #   PadUtils.file_exist?("path/to/file.txt")
   def self.file_exist?(file_path)
     File.exist?(file_path)
   end
 
-  # Just a wrapper on FileUtils.cp for consistency.
-  # Will override file if it already exists!
+  # Copies a file.
+  #
+  # @param file_path [String] the source file name and path
+  # @param dest_dir [String] the destination directory
+  # @return [Void] nothing
+  # @example
+  #   PadUtils.copy_file("test.txt", "path/to/destination/directory")
   def self.copy_file(file_path, dest_dir)
     FileUtils.cp(file_path, dest_dir)
   end
 
-  # Just a wrapper on FileUtils.mv for consistency.
-  # Will not throw an error if original file doesn't exist
+
+  # Moves a file.
+  #
+  # @param file_path [String] the source file path and name
+  # @param dest_file [String] the destination path and name
+  # @return [Void] nothing
+  # @example
+  #   PadUtils.move_file("source.txt", "path/to/destination/renamed.txt")
   def self.move_file(file_path, dest_file)
     FileUtils.mv(file_path, dest_file, force: true)
   end
 
-  # Copy an array of files
+  # Copies an array of files.
+  #
+  # @param files [Array<String>] the array of files paths and names to copy
+  # @param dest_dir [String] the destination path
+  # @return [Void] nothing
+  # @example
+  #   files = ["file1.txt", "path/to/file2.txt", "path/to/files/file3.txt"]
+  #   PadUtils.copy_files(files, "path/to/destination")
   def self.copy_files(files, dest_dir)
     files.each do |f|
       copy_file(f, dest_dir)
     end
   end
 
-  # Copy all files from a directory to another.
-  # Will create destination if it doesn't exist
+  # Copies all files from a directory.
+  #
+  # @note Will create the destination if it doesn't exist.
+  #   Files existing on destination but not on source won't be overwritten.
+  #
+  # @param orig_dir [String] the path to source directory
+  # @return [Void] nothing
+  # @example
+  #   PadUtils.copy_all_files("path/to/source", "path/to/destination")
   def self.copy_all_files(orig_dir, dest_dir)
     FileUtils.copy_entry(orig_dir, dest_dir)
   end
 
-  # Create a directory and subdirectories
-  # Won't complain if it already exists. Won't override content.
+  # Creates a directory.
+  #
+  # @note Won't override directory content if it already exists.
+  #
+  # @param dir_name [String] the directory path and name
+  # @return [Void] nothing
+  # @example
+  #   PadUtils.create_directory("path/to/dir")
   def self.create_directory(dir_name)
     FileUtils.mkdir_p(dir_name)
   end
 
-  # Delete a directory and its content.
-  # Just a wrapper for consistency.
+  # Deletes a directory and its content
+  #
+  # @param dir_name [String] the directory path and name
+  # @return [Void] nothing
+  # @example
+  #   PadUtils.delete_directory("path/to/dir")
   def self.delete_directory(dir_name)
     FileUtils.rm_r(dir_name)
   end
 
-  # Reads content of a file. Method created for consistency.
+  # Reads the whole content of a file into a string.
+  #
+  # Will log errors using {PadUtils.log PadUtils.log}.
+  #
+  # @param filepath [String] the file path and name
+  # @return [String] the content of the file
+  # @example
+  #   PadUtils.get_file_content("path/to/file.txt")
   def self.get_file_content(filepath)
     File.read(filepath)
   rescue Exception => e
     PadUtils.log("Error in get_file_content", e)
   end
 
-  # Write content to a file. Create it if it doesn't exist.
+  # Writes content to a file.
+  #
+  # @note Will create destination file if it doesn't exist.
+  #   Will also overwrite the file if it already exists.
+  #
+  # Will log errors using {PadUtils.log PadUtils.log}.
+  #
+  # @param  filepath [String] the file path and name
+  # @param  content [String] the content to be written
+  # @return [Void] nothing
+  # @example
+  #   PadUtils.write_to_file("path/to/file", "Hello\nThis is a test")
   def self.write_to_file(filepath, content)
     File.open(filepath, 'w') { |f| f.write(content)}
   rescue Exception => e
     PadUtils.log("Error in write_to_file", e)
   end
 
-  # Append content to the end of a file. Create it if it doesn't exist.
-  # It will write a newline character first. If you don't want that,
-  # set the new_line option to false.
+
+  # Appends content to a file.
+  #
+  # @note Will create the destination file if it doesn't exist.
+  #   It also prepends a newline character. Set new_line to false to change this.
+  #
+  # Will log errors using {PadUtils.log PadUtils.log}.
+  #
+  # @param file_path [String] the file path and name
+  # @param content [String] the content to append
+  # @param new_line [Boolean] prepends a newline character
+  # @return [Void] nothing
+  # @example
+  #   PadUtils.append_to_file("path/to/file.txt", "Append this!", false)
   def self.append_to_file(filepath, content, new_line = true)
     content = "\n#{content}" if new_line
     File.open(filepath, 'a') { |f| f.write("#{content}")}
--- lib/pad_utils/version.rb
+++ lib/pad_utils/version.rb
@@ -1,3 +1,4 @@
 module PadUtils
+  # PadUtils version number  
   VERSION = "1.2.1"
 end
