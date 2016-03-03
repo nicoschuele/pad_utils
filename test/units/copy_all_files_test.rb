51a96c75e5e877fcae36ecd1b4276d8c7fc67470
Nico Schuele -mini <nicoschuele@gmail.com>
2016-03-03 19:03:13 +0100

    Revert to copy_entry for copying_all_files

---
+++ test/units/copy_all_files_test.rb
@@ -0,0 +1,30 @@
+require 'pad_utils'
+require_relative '../template/test'
+
+# Test name
+test_name = "CopyAllFiles"
+
+class CopyAllFilesTest < Test
+
+  def prepare
+    # Add test preparation here
+  end
+
+  def run_test
+    # Test code for CopyAllFiles goes here
+    #
+    # Runtime errors will be handled by a Rescue in the parent class
+    #
+    # You can add error messages to @errors
+    #   example: @errors << "Some error message"
+    # You can also add notes to @notes
+    #   example: @notes << "Some note"
+    @notes << "Execution directory: #{Dir.pwd}"
+    PadUtils.copy_all_files "fixtures/source", "results"
+  end
+
+  def cleanup
+    # Add cleanup code here
+  end
+
+end
