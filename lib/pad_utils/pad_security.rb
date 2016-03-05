1467a802ee8d8ca084af0822e22104ad6b5d4808
Nico Schuele -mini <nicoschuele@gmail.com>
2016-03-05 20:01:13 +0100

    Add doc for encrypt/decrypt

--- lib/pad_utils/pad_security.rb
+++ lib/pad_utils/pad_security.rb
@@ -5,47 +5,13 @@ require 'digest/sha1'
 
 module PadUtils
 
-  # def self.encrypt(content: "", key: "", iv: "7478110410030520")
-  #   cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
-  #   cipher.encrypt
+  # Encrypts a string.
   #
-  #   key = Digest::SHA1.hexdigest(key)
-  #
-  #   cipher.key = key
-  #   cipher.iv = iv
-  #
-  #   encrypted = '' << cipher.update(content) << cipher.final
-  #
-  #   encrypted = Base64.encode64(encrypted)
-  #   encrypted = URI.escape(encrypted, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-  #
-  #   encrypted
-  #
-  # rescue
-  #   encrypted = "invalid"
-  # end
-  #
-  # def self.decrypt(content: "", key: "", iv: "7478110410030520")
-  #   cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
-  #   cipher.decrypt
-  #
-  #   key = Digest::SHA1.hexdigest(key)
-  #
-  #   cipher.key = key
-  #   cipher.iv = iv
-  #
-  #   content = URI.unescape(content)
-  #   content = Base64.decode64(content)
-  #
-  #   decrypted = '' << cipher.update(content) << cipher.final
-  #
-  #
-  #   decrypted
-  #
-  # rescue
-  #   decrypted = "invalid"
-  # end
-
+  # @param content [String] the content to encrypt
+  # @param key [String] the encryption key
+  # @return [String] the encrypted string
+  # @example
+  #   PadUtils.encrypt("Hello", "s3cr3t") # => 'DKBJVYG69YsAWT%0A__1f346a8eeedc7'
   def self.encrypt(content: "", key: "")
     cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
     cipher.encrypt
@@ -68,6 +34,13 @@ module PadUtils
     encrypted = "invalid"
   end
 
+  # Decrypts a string.
+  #
+  # @param content [String] the encrypted string
+  # @param key [String] the encryption key
+  # @return [String] the decrypted string
+  # @example
+  #   PadUtils.decrypt("DKBJVYG69YsAWT%0A__1f346a8eeedc7", "s3cr3t") # => 'Hello'
   def self.decrypt(content: "", key: "")
     cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
     cipher.decrypt
@@ -84,7 +57,6 @@ module PadUtils
     content = Base64.decode64(content)
 
     decrypted = '' << cipher.update(content) << cipher.final
-
 
     decrypted
 
