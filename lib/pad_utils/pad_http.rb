fdd5d319535939db8913904434b60e196bbed037
Nico Schuele -mini <nicoschuele@gmail.com>
2016-03-06 01:56:22 +0100

    Add HTTP get methods

--- lib/pad_utils/pad_http.rb
+++ lib/pad_utils/pad_http.rb
@@ -20,10 +20,10 @@ module PadUtils
   #   url = "http://example.com/api/movies"
   #   body = {year: 2008, director: "Nolan"}
   #   PadUtils.http_post(url: url, body: body) # => {movie: "The Dark Knight"}
-  def self.http_post(url: "", body: {}, headers: {'Content-Type' => 'application/json', 'User-Agent' => 'Padstone'})
+  def self.http_post(url: "", body: {}, headers: {'User-Agent' => 'Padstone'})
     reply_hash = {}
     body = PadUtils.hash_to_json(body)
-    reply = HTTParty.post(url, {headers: headers, body: body})
+    reply = HTTParty.post(url, {headers: headers, body: body}).to_s
     reply_hash = PadUtils.json_to_hash reply
     reply_hash
   rescue JSON::ParserError => e
@@ -34,8 +34,52 @@ module PadUtils
     reply_hash = {error: e.message}
   end
 
-  # def self.http_get(url: "", header: {'Content-Type' => 'application/json'})
+  # Sends a HTTP GET request and receives plain text.
   #
-  # end
+  #
+  # If the server can't be reached, `"Server unreachable"` is returned.
+  #
+  # @param url [String] the url to call
+  # @param headers [Hash] the hash containing the header key/value pairs
+  # @return [String] the response
+  # @example
+  #   url = "http://example.com/api/joke"
+  #   PadUtils.http_get_plain(url) # => "There are 10 kinds of programmers."
+  def self.http_get_plain(url, headers: {'User-Agent' => 'Padstone'})
+    reply = HTTParty.get(url, headers: headers).to_s
+  rescue Errno::ECONNREFUSED => e
+    reply = "Server unreachable"
+  rescue Exception => e
+    reply = e.message
+  end
+
+  # Sends a HTTP GET request and receives a hash.
+  #
+  # @note Although it can parse any JSON responses, this method is made to work
+  #   in conjunction with PadUtils on the server side code.
+  #
+  # If the server can't be reached, `{error: "Server unreachable"}` is returned.
+  #
+  # If the JSON in the response can't be parsed (or if it's not JSON),
+  #   `{error: "Response is not JSON"}` is returned.
+  #
+  # @param url [String] the url to call
+  # @param headers [Hash] the hash containing the header key/value pairs
+  # @return [Hash] the response as a hash
+  # @example
+  #   url = "http://example.com/api/weather"
+  #   PadUtils.http_get(url) # => {Geneva: "Sunny", Lausanne: "Cloudy"}
+  def self.http_get(url, headers: {'User-Agent' => 'Padstone'})
+    reply_hash = {}
+    reply = HTTParty.get(url, headers: headers).to_s
+    reply_hash = PadUtils.json_to_hash reply
+    reply_hash
+  rescue JSON::ParserError => e
+    reply_hash = {error: "Response is not JSON"}
+  rescue Errno::ECONNREFUSED => e
+    reply_hash = {error: "Server unreachable"}
+  rescue Exception => e
+    reply_hash = {error: e.message}
+  end
 
 end
--- test/units/http_get_test.rb
+++ test/units/http_get_test.rb
@@ -1,38 +1,78 @@
-# require 'pad_utils'
-# require_relative '../template/test'
-#
-# # Test name
-# test_name = "HttpGet"
-#
-# class HttpGetTest < Test
-#
-#   def prepare
-#     # Add test preparation here
-#   end
-#
-#   def run_test
-#     # Test 1
-#     puts "Test 1:"
-#     url = "http://localhost:3000/services/v1/test/index"
-#     reply = PadUtils.http_get(url)
-#
-#     PadUtils.puts_c "Calling: #{url}", :blue
-#     PadUtils.puts_c "Reply: #{reply}", :blue
-#
-#     if reply == "Hello"
-#       @notes << "Response received"
-#     else
-#       @error << "Something went wrong"
-#     end
-#
-#     puts
-#
-#     # Test 2
-#
-#   end
-#
-#   def cleanup
-#     # Add cleanup code here
-#   end
-#
-# end
+require 'httparty'
+require 'pad_utils'
+require_relative '../template/test'
+
+# Test name
+test_name = "HttpGet"
+
+class HttpGetTest < Test
+
+  def prepare
+    # Add test preparation here
+  end
+
+  def run_test
+    # Test 1
+    puts "Test 1:"
+    url = "http://localhost:3000/services/v1/test/index"
+    reply = PadUtils.http_get_plain(url)
+
+    PadUtils.puts_c "Calling: #{url}", :blue
+    PadUtils.puts_c "Reply: #{reply}", :blue
+    PadUtils.puts_c "Reply class: #{reply.class}", :blue
+
+    if reply == "Hello"
+      @notes << "Response received"
+    elsif reply == "Server unreachable"
+      @notes << "#{reply}"
+    else
+      @errors << "Something went wrong"
+    end
+
+    puts
+
+    # Test 2
+    puts "Test 2:"
+    url = "http://localhost:3000/services/v1/test/index_json"
+    reply = PadUtils.http_get(url)
+
+    PadUtils.puts_c "Calling: #{url}", :blue
+    PadUtils.puts_c "Reply: #{reply}", :blue
+    PadUtils.puts_c "Reply class: #{reply.class}", :blue
+
+    if reply[:message] == "Hello"
+      @notes << "Response received"
+    elsif !reply[:error].nil?
+      @notes << "#{reply[:error]}"
+    else
+      @errors << "Something went wrong"
+    end
+
+    puts
+
+    # Test 3
+    puts "Test 3:"
+    url = "http://localhost:3000/services/v1/test/index_error"
+    reply = PadUtils.http_get(url)
+
+    PadUtils.puts_c "Calling: #{url}", :blue
+    PadUtils.puts_c "Reply: #{reply}", :blue
+    PadUtils.puts_c "Reply class: #{reply.class}", :blue
+
+    if reply[:message] == "Hello"
+      @notes << "Response received"
+    elsif !reply[:error].nil?
+      @notes << "#{reply[:error]}"
+    else
+      @errors << "Something went wrong"
+    end
+
+    puts
+
+  end
+
+  def cleanup
+    # Add cleanup code here
+  end
+
+end
